e784a12ac847e9c7a92839ca8afdd5349597b9d6
ytqaljn <2716693942@qq.com>
2023-04-20 00:45:49 -0700

    test: test ed25519 end, and back

--- primitives/io/src/lib.rs
+++ primitives/io/src/lib.rs
@@ -782,7 +782,6 @@ pub trait Crypto {
 			.unwrap_or_default()
 		{
 			use ed25519_dalek::Verifier;
-			log::info!("select 1");
 			let public_key = if let Ok(vk) = ed25519_dalek::PublicKey::from_bytes(&pub_key.0) {
 				vk
 			} else {
@@ -793,27 +792,8 @@ pub trait Crypto {
 
 			public_key.verify(msg, &sig).is_ok()
 		} else {
-			log::info!("select 2");
 			ed25519::Pair::verify(sig, msg, pub_key)
 		}
-	}
-
-	fn cess_ed25519_verify_1(sig: &ed25519::Signature, msg: &[u8], pub_key: &ed25519::Public) -> bool {
-		ed25519::Pair::verify(sig, msg, pub_key)
-	}
-
-	fn cess_ed25519_verify_2(sig: &ed25519::Signature, msg: &[u8], pub_key: &ed25519::Public) -> bool {
-		use ed25519_dalek::Verifier;
-		
-		let public_key = if let Ok(vk) = ed25519_dalek::PublicKey::from_bytes(&pub_key.0) {
-			vk
-		} else {
-			return false
-		};
-
-		let sig = ed25519_dalek::Signature::from(sig.0);
-
-		public_key.verify(msg, &sig).is_ok()
 	}
 
 	/// Register a `ed25519` signature for batch verification.
